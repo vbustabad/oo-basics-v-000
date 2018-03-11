7731bf62a7dcafd8e65ac78f46288b1e930c9fc9
Veronica Bustabad <vbustabad@gmail.com>
2018-03-11 03:21:52 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -19,9 +19,17 @@ class Book
   def page_count=(num)
     @page_count = num
   end
-   
+
   def page_count
     @page_count
+  end
+
+  def genre=(genre)
+     @genre = genre
+  end
+  
+  def genre
+     @genre
   end
 
 end
