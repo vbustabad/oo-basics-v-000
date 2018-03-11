afde11c2d347f50402fc43a1ffb8f91666390f65
Veronica Bustabad <vbustabad@gmail.com>
2018-03-11 03:49:17 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,11 @@
-# Make your shoe class here!
+class Shoe
+  attr_accessor :brand, :color, :size, :material, :condition
+  
+  def initialize(title)
+    @title = title
+  end
+
+  def title
+    @title
+  end
+
